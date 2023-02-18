e3caf25075ac17b32a5d8b7b5c2bdd47d8802a99
visels <elviskipchumba@yahoo.com>
2023-02-18 16:48:27 +0300

    configured connection to database

---
+++ src/main/java/com/visels/main/Testing/Domain/Product.java
@@ -0,0 +1,17 @@
+package com.visels.main.Testing.Domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String amount;
+    private String description;
+}
